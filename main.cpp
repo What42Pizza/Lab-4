a3995c79b74f4724444f76700e2c936ddb056092
Jordan Johnson <jordanjohnson@Jordans-MacBook-Air.local>
2026-09-21 12:33:08 -0500

    started working on Lab 4

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
 int main() {
-	
+	string foodName;
+    cin >> foodName;
+    char itemCode;
+    cin >> itemCode;
+    int itemQuantity;
+    cin >> itemQuantity;
+    double unitPrice;
+    cin >> unitPrice;
+    bool isMember;
+    cin >> isMember;
+
 }
